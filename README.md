e383198ae50936d3e9fa39c27d23b5c4a87f543f
PhilHarmon1 <philineshiaharmon@icloud.com>
2020-01-29 14:54:14 -0500

    added js

--- README.md
+++ README.md
@@ -10,9 +10,8 @@ here is a list of thngs i am passionate about
 - love
 
 My Daughter name is *Jordyn Rogers* :heart_eyes:
+https://apis.mail.yahoo.com/ws/v3/mailboxes/@.id==VjN-IN7-oDyhLcMTTsjXA2r7MZbeK5Ou6KPhkx8Q8nrsAPSlRXW3Wr2G5HV_QzvGmVHmIeZ9r3PtPZxOW6HfvodQfg/messages/@.id==AEZBRi5idGryXjCE4gYK4PvNy-s/content/parts/@.id==2/thumbnail?appId=YMailNorrinLaunch
 
-  https://apis.mail.yahoo.com/ws/v3/mailboxes/@.id==VjN-IN7-oDyhLcMTTsjXA2r7MZbeK5Ou6KPhkx8Q8nrsAPSlRXW3Wr2G5HV_QzvGmVHmIeZ9r3PtPZxOW6HfvodQfg/messages/@.id==AEZBRi5idGryXjCE4gYK4PvNy-s/content/parts/@.id==2/thumbnail?appId=YMailNorrinLaunch
-  
 # TO Do List 
 1. Get Food
 1. Get Hair done 
---
+++ script.js
@@ -0,0 +1,47 @@
+// going to lunch 
+var haveMoney = true;
+var fastFood = false;
+var homeMade= "No";
+var type = "wings";
+var resturant = "chinese";
+
+if (haveMoney){
+if (resturant){
+if (type === "wings"){
+   console.log("Chinese");
+  } else is (type === "burger")
+     console.log("5Guys");
+} else if (type === "salad")
+     console.log("Top Dog");               
+  
+} else if (homeMade === "yes"){
+  console.log("goHome");
+} else if (homeMade === "dont like it"){
+  console.log("aska friend");
+} else {
+  console.log("starve");
+}
+
+console.log(2+2);
+
+var p = "8"; 
+var h = "4";
+var i = "9";
+var l = "3";
+
+
+console.log(p + i);
+console.log(h + l);
+
+// my name
+ var first = "philineshia";
+ var middle = "lashay";
+ var last = "harmon";
+ console.log(first*middle*last);
+
+ var counts = 70;
+ var name = "Phil";
+ console.log(counts*name);
+
+
+
